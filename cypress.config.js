35278b4b2b2782bc0c7b63977bcd7864deb8eb5e
ntraspaderne <nicotrasparrea@gmail.com>
2023-08-30 14:06:35 +0200

    added allure reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,34 +1,29 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
-const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      on("file:preprocessor",
-        createBundler({
-          plugins: [createEsbuildPlugin.default(config)],
-        })
-      );
-      preprocessor.addCucumberPreprocessorPlugin(on, config);
-      require('cypress-mochawesome-reporter/plugin')(on);
-      // Make sure to return the config object as it might have been modified by the plugin.
+    async setupNodeEvents(cypressOn, config) {
+
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      });
+
+      cypressOn("file:preprocessor", bundler);
+      const on = require('cypress-on-fix')(cypressOn)
+      allureWriter(on, config);
+      await addCucumberPreprocessorPlugin(on, config);
       return config;
     },
-    specPattern: '**/*.feature',
-    excludeSpecPattern: '**/pages/*',
-    videosFolder: "cypress/reports/videos",
-    screenshotsFolder: "cypress/reports/screenshots",
-    reporter: 'cypress-mochawesome-reporter',
-    reporterOptions: {
-      charts: true,
-      reportPageTitle: 'cypress report test',
-      embeddedScreenshots: true,
-      inlineAssets: true,
-      saveAllAttempts: false,
+    env: {
+      allureReuseAfterSpec: true
     },
-    projectId: "vs37wz"
-
+    specPattern: "cypress/e2e/features/*.feature",
+    baseUrl: "http://devtodo.monfared.io/",
+    chromeWebSecurity: false,
+    projectId: "vs37wz",
   },
 });
